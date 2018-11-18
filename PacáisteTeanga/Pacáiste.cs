43ab81d12c29201f4f2b32c0d219d0c1bd7f2429
Jeffrey Sardina <jeffrey.sardina@gmail.com>
2018-11-17 23:12:36 -0800

    D'Athraigh mé an cód "Lódáileor" go "Pacáiste" agus nua-shonraigh mé an cód atá ann (anois is féidir leis comhaid .cvs a úsáid)

---
+++ PacáisteTeanga/Pacáiste.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+/// <summary>
+/// Seo é cód le pacáiste teanga. Tá sé in ann:
+///     comhaid pacáiste teanga i bhformáid .csv a oscailt
+///     frásaí i dteangacha éagsúil a lódáil chun códuimhir uathúil
+///     
+/// Caitheann gach comhad anim na teanga (no cód na teanga) a beith aige.
+///     Mar shampla, beidh anim an pacáiste Gaeilge "Gaeilge.csv" nó "ga.csv"
+///     Formáid an chomhaid:
+///         Códfhocal-no-Códuimhir,Focal-nó-frása
+///         ...
+///     Mar shampla:
+///         1,Dia duit
+///         134,Dia daoibh
+///         8345,Conas atá sibh? Tá mé go maith
+///         [Deireadh]
+/// </summary>
+namespace PacáisteTeanga
+{
+    /// <summary>
+    /// Seo é an cód a lódáileann comhaid teanga ón ríomhaire.
+    /// </summary>
+    public class Pacáiste
+    {
+        readonly Dictionary<int, string> nascaireacht;
+        public string Teanga
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Déanann sé seo Pacáiste nua chun an comhad teanga atá aimsithe
+        /// </summary>
+        /// <param name="comhad">
+        /// An comhad lena teangacha i bhformáid ceart (féach an tuarscáil atá suas ag barr an comhad)
+        /// </param>
+	    public Pacáiste(string comhad)
+        {
+            nascaireacht = new Dictionary<int, string>();
+            ComhadALódáil(comhad);
+        }
+
+        /// <summary>
+        /// Lódáileann sé seo an comhad i bhfoclóir frásaí
+        /// </summary>
+        /// <param name="comhad">
+        /// An comhad leis an teanga i bhformáid .csv
+        /// </param>
+        void ComhadALódáil(string comhad)
+        {
+            //anim na teanga (gan an fadú .csv)
+            Teanga = comhad.Split('.')[0];
+
+            //Lódáil gach frása i nascaireachtTeangacha
+            try
+            {
+                foreach (string líne in File.ReadLines(comhad))
+                {
+                    //Tá dhá phíosa eolais sa líne: an cód agus an frása
+                    string[] eolas = líne.Split(',');
+
+                    //Tá eolas[0] an cód agus eolas[1] an frása
+                    int cód = Int32.Parse(eolas[0]);
+                    string frása = eolas[1];
+                    nascaireacht.Add(cód, frása);
+                }
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                throw new Exception("Níl formáid maith ag an gcomhad seo\n");
+            }
+        }
+
+        /// <summary>
+        /// Athraíonn sé seo an teanga ar fáil
+        /// </summary>
+        /// <param name="comhad">
+        /// An comhad leis an teanga i bhformáid .csv
+        /// </param>
+        public void TeangaAAthrú(string comhad)
+        {
+            nascaireacht.Clear();
+            ComhadALódáil(comhad);
+        }
+
+        /// <summary>
+        /// Fillean sé seo frása an chóid faighte
+        /// </summary>
+        /// <param name="cód">Cód na fhrása teastaithe</param>
+        /// <returns>
+        /// Frása leis an cód seo, nó "null" mura bhfuil aon frása aimsithe
+        /// </returns>
+        public string FrásaAFháil(int cód)
+        {
+            nascaireacht.TryGetValue(cód, out string frása);
+            return frása;
+        }
+    }
+}
